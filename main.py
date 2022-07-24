32f2155ede19ae733c8a5d098b47420717cfc508
Omkar Patel <106450097+omkarxpatel@users.noreply.github.com>
2022-07-24 15:08:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
-import sys
-import discord
-import traceback
-from pathlib import Path
-from colorama import Fore
 from discord.ext import commands
 
-intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=["-"],intents=intents,activity=discord.Activity(type=discord.ActivityType.listening, name="To The Almighty Thug Lord"))
-
 
 @bot.event
 async def on_ready():
-  print(Fore.MAGENTA + "========[ " + bot.user.name + " is Online! ]========" + Fore.RESET)
-  print(Fore.RED + "========[ Discord Version: " + discord.__version__ + " ]=======")
-      
-bot.run(os.getenv("TOKEN"))
+    print(f"Logged in as {bot.user.name}({bot.user.id})")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+if __name__ == "__main__":
+    bot.run(os.getenv("TOKEN"))
